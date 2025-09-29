1e9f8e87ee8a9e67b70e949795a589e658e51c3d
Marco Melloni <98281551+marcomelloni@users.noreply.github.com>
2025-09-29 20:24:27 +0200

    Add REST protocol adapter for simulation bridge

--- python-mock-physical-twin/app/protocol/__init__.py
+++ python-mock-physical-twin/app/protocol/__init__.py
@@ -4,9 +4,11 @@
 from .http_protocol import HttpProtocol
 from .mqtt_protocol import MqttProtocol
 from .simulation_bridge_amqp_protocol import SimulationBridgeAmqpProtocol
+from .simulation_bridge_rest_protocol import SimulationBridgeRestProtocol
 
 __all__ = [
     "HttpProtocol",
     "MqttProtocol",
     "SimulationBridgeAmqpProtocol",
+    "SimulationBridgeRestProtocol",
 ]
